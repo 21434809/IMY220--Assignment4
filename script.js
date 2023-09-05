d7fafb62eea88496bb932dfe4392777717966b4a
u21434809 <u21434809@tuks.co.za>
2023-09-05 16:28:56 +0200

    fixed multi link issue bonus

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ $(document).ready(function() {
         $("#right").click(function() {
             let message = $("#message").val();
             let  matches = message.match(youtubeUrlRegex);
-            if(matches){
-                message = message.replace(youtubeUrlRegex, `<div class='embed-responsive embed-responsive-16by9'><iframe class='embed-responsive-item' src='https://www.youtube.com/embed/${matches[1]}' allowfullscreen></iframe></div>`);
-                }
+            while(matches){
+                message = message.replace(youtubeUrlRegex, `<div class='embed-responsive embed-responsive-16by9 mb-2'><iframe height="315" class='embed-responsive-item' src='https://www.youtube.com/embed/${matches[1]}' allowfullscreen></iframe></div>`);
+                matches = message.match(youtubeUrlRegex);
+            }
             if(message.length > 0){
                 $(".messages").append(`<div class='row justify-content-center'><div class='col-9 col-lg-4  alert alert-primary' role='alert' style='word-wrap: break-word;'>${message}</div></div>`);
             }
@@ -14,8 +15,9 @@ $(document).ready(function() {
         $("#left").click(function() {
             let message = $("#message").val();
             let  matches = message.match(youtubeUrlRegex);
-            if(matches){
-                message = message.replace(youtubeUrlRegex, `<div class='embed-responsive embed-responsive-16by9'><iframe class='embed-responsive-item' src='https://www.youtube.com/embed/${matches[1]}' allowfullscreen></iframe></div>`);
+            while(matches){
+                message = message.replace(youtubeUrlRegex, `<div class='embed-responsive embed-responsive-16by9'><iframe height="315" class='embed-responsive-item' src='https://www.youtube.com/embed/${matches[1]}' allowfullscreen></iframe></div>`);
+                matches = message.match(youtubeUrlRegex);
                 }
             if(message.length > 0){
                 $(".messages").append(`<div class='row justify-content-center'><div class='col-9 col-lg-4  alert alert-success' role='alert' style='word-wrap: break-word;'>${message}</div></div>`);
